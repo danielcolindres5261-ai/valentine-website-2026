bcca3922b409f29888840b024ca00a66d0fb1c25
danielcolindres5261-ai <danielcolindres5261@gmail.com>
2026-02-07 13:54:30 -0600

    Update config.js

--- config.js
+++ config.js
@@ -42,9 +42,9 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "A LAAAAAAAAAAAA ENSERIO TANTO?? 🥰🚀💝",  // Shows when they go past 5000%
+        high: "HASTA EL INFINITO Y MAS ALLAAAA! 🚀💝",              // Shows when they go past 1000%
+        normal: "Y TODAVIA MASSSSSSSSSSS! 🥰"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
