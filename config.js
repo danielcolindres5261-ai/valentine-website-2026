8ec628717af617e232806d3fe7384833e68ef5c5
danielcolindres5261-ai <danielcolindres5261@gmail.com>
2026-02-07 14:02:41 -0600

    Update config.js

--- config.js
+++ config.js
@@ -81,7 +81,7 @@ const CONFIG = {
         musicUrl: "https://youtu.be/2caC0IQF1vg", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
-        volume: 0.5                        // Volume level (0.0 to 1.0)
+        volume: 1.0                        // Volume level (0.0 to 1.0)
     }
 };
 
